6244a2d0f9f50e6b4f19c9305526bbb4228220ed
Oleg <keeperoleg26@gmail.com>
2024-04-11 16:59:15 +0300

    ogoogle forms

--- src/components/contactUs/index.tsx
+++ src/components/contactUs/index.tsx
@@ -1,29 +1,63 @@
-import "./index.scss";
-import { Input, Button } from "antd";
-import {
-  faUpwork,
-  faInstagram,
-  faFacebookF,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
-import axios from "axios";
+import './index.scss';
+import { Input, Button } from 'antd';
+import { faUpwork, faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import axios from 'axios';
 
 export function ContactUs() {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [text, setText] = useState("");
-  const onSubmit = () => {
-    const body = {
-      name,
-      lastName,
-      email,
-      text,
-    };
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [text, setText] = useState('');
+  const [showForm, setShowForm] = useState(true);
 
-    axios.post("/email", body);
+  const onSubmit = () => {
+    const data = {
+      'entry.1678961923': name,
+      'entry.1764846499': lastName,
+      'entry.1578438204': email,
+      'entry.107176989': text,
+    };
+    setName('');
+    setLastName('');
+    setEmail('');
+    setText('');
+    setShowForm(false);
+    axios({
+      data,
+      method: 'POST',
+      url: import.meta.env.VITE_GOOGLE_FORM,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }).catch(console.log);
   };
+
+  const renderForm = () => (
+    <div className="form">
+      <div className="form-item duplex">
+        <Input placeholder="Name" value={name} onChange={(ev) => setName(ev.target.value)} />
+        <Input placeholder="Last Name" value={lastName} onChange={(ev) => setLastName(ev.target.value)} />
+      </div>
+      <div className="form-item">
+        <Input type="email" placeholder="Email" value={email} onChange={(ev) => setEmail(ev.target.value)} />
+      </div>
+      <div className="form-item text">
+        <Input
+          value={text}
+          onChange={(ev) => setText(ev.target.value)}
+          placeholder="Please tell us a bit  about your goals, what are you hoping  to  achieve with NSTsoft"
+        />
+      </div>
+      <div className="form-item ">
+        <Button className="submit" onClick={onSubmit}>
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <section>
       <div className="ContactUs" id="contact-us">
@@ -33,8 +67,7 @@ export function ContactUs() {
               <img width={140} src="./assets/transparent_full.png" />
             </div>
             <div className="text">
-              A software product development company located in Ukraine, with
-              wide range of technology solutions.
+              A software product development company located in Ukraine, with wide range of technology solutions.
             </div>
             <div className="email">Contact email: info@nstsoft.com</div>
             <div className="social">
@@ -43,40 +76,7 @@ export function ContactUs() {
               <FontAwesomeIcon className="icon" icon={faFacebookF} />
             </div>
           </div>
-          <div className="form">
-            <div className="form-item duplex">
-              <Input
-                placeholder="Name"
-                value={name}
-                onChange={(ev) => setName(ev.target.value)}
-              />
-              <Input
-                placeholder="Last Name"
-                value={lastName}
-                onChange={(ev) => setLastName(ev.target.value)}
-              />
-            </div>
-            <div className="form-item">
-              <Input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(ev) => setEmail(ev.target.value)}
-              />
-            </div>
-            <div className="form-item text">
-              <Input
-                value={text}
-                onChange={(ev) => setText(ev.target.value)}
-                placeholder="Please tell us a bit  about your goals, what are you hoping  to  achieve with NSTsoft"
-              />
-            </div>
-            <div className="form-item ">
-              <Button className="submit" onClick={onSubmit}>
-                Submit
-              </Button>
-            </div>
-          </div>
+          {showForm ? renderForm() : <div className="reply">Thanks for Your reply</div>}
         </div>
       </div>
     </section>
